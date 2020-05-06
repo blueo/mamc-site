33b623c3ef9b4aef07ddb3982f8a49ae3ec78b7c
Bernard Hamlin <bernie@silverstripe.com>
2020-05-06 20:31:01 +1200

    news layout

--- portfolio/.vuepress/theme/layouts/NewsLayout.vue
+++ portfolio/.vuepress/theme/layouts/NewsLayout.vue
@@ -3,30 +3,16 @@
 
     <Navbar :logo="$site.themeConfig.logo" :sticky="$route.path === '/'" />
 
-    <div class="container">
+    <main class="container">
+      <!-- Single news item view -->
+      <section>
+        <h1>{{ $page.frontmatter.title }}</h1>
+        <small>{{ $page.frontmatter.date }}</small>
 
-news layout
-      <!-- Single project view -->
-      <div v-if="isSingleProject">
-        <SingleProjectHeader
-          :title="$page.frontmatter.title"
-          :year="$page.frontmatter.year.toString()"
-          :categories="$page.frontmatter.categories"
-        />
-        <Content/>
-      </div>
+        {{ $page.frontmatter.body }}
+      </section>
 
-      <!-- Journal list -->
-      <div v-if="$route.path === '/journal/'" class="journal-list">
-        <Content />
-      </div>
-
-      <!-- Single journal -->
-      <div v-if="isSingleJournal" class="single-journal">
-        <Content/>
-      </div>
-
-    </div>
+    </main>
 
     <Footer />
 
@@ -35,39 +21,7 @@ news layout
 
 <script>
   export default {
-    computed: {
-      isSingleProject() {
-        const worksRoute = '/works/'
-        const path = this.$route.path
-        if (path.includes('works') && path.length >= (worksRoute.length + 1)) {
-          return true
-        }
-      },
-      isSingleJournal() {
-        const journalRoute = '/journal/'
-        const path = this.$route.path
-        if (path.includes('journal') && path.length >= (journalRoute.length + 1)) {
-          return true
-        }
-      }
-    },
-    updated() {
-        // unwrap all images from paragraph tags so we can have
-        // different widths inside the content.
-
-        document.querySelectorAll('p img').forEach(image => {
-          var wrapper = image.parentNode
-          let children = wrapper.children
-          let fragment = document.createDocumentFragment()
-
-          Array.from(children).forEach(child => {
-            fragment.appendChild(child)
-          })
-
-          wrapper.parentNode.replaceChild(fragment, wrapper)
-
-        })
-    },
+    computed: {},
   }
 </script>
 
--- portfolio/news/first-news.md
+++ portfolio/news/first-news.md
@@ -5,4 +5,3 @@ date: 2020-05-02 12:00:00 +0000
 layout: NewsLayout
 
 ---
-test here
