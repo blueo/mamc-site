e881a69ed8386c9415b61ab2a2dbe34a6551c7d7
Bernie Hamlin <bernard.hamlin@gmail.com>
2020-11-25 21:19:32 +1300

    Add basic content block

---
+++ site/.vuepress/components/blocks/ContentBlock.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row">
+    <div class="container">
+      <section class="content-block">
+        <div class="content">
+          <h2 v-if="title">{{ title }}</h2>
+          <p>
+            {{ content }}
+          </p>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="stylus">
+h2 {
+  color: var(--color-bluegreen);
+}
+
+.content-block {
+  display: flex;
+  flex-direction: column;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+}
+</style>
--- site/.vuepress/components/blocks/block-map.js
+++ site/.vuepress/components/blocks/block-map.js
@@ -1,10 +1,12 @@
 import MediaBlock from './MediaBlock';
 import CallToActionBlock from './CallToActionBlock';
 import CenteredMediaBlock from './CenteredMediaBlock';
+import ContentBlock from './ContentBlock';
 
 // mapping of forestry block types to components
 export default {
     'media-block': MediaBlock,
     'call-to-action-block': CallToActionBlock,
-    'centered-media-block': CenteredMediaBlock
+    'centered-media-block': CenteredMediaBlock,
+    'content-block': ContentBlock,
 };
