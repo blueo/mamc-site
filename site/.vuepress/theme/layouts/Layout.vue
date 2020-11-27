3041f4e1c1d0408341d710fe0d99f7ccd7bc8f40
Bernie Hamlin <bernard.hamlin@gmail.com>
2020-11-27 21:33:25 +1300

    change directions to a tag

--- site/.vuepress/theme/layouts/Layout.vue
+++ site/.vuepress/theme/layouts/Layout.vue
@@ -228,4 +228,14 @@ textarea, input {
   font-family: $font-family-heading;
   display: inline-block;
 }
+
+.visually-hidden {
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+}
 </style>
